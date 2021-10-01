6eedd5482b56ea799e2710729c9455a42000b197
Komal Shaw <shawkomal55@gmail.com>
2021-10-01 18:44:21 +0530

    Added Kadane's Algorithm in cpp

---
+++ cpp/kadanealgo.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int max_so_far = INT_MIN;
+        int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            if(max_so_far<sum)
+            max_so_far = sum;
+            if(sum<0)
+            sum=0;
+        }
+        return max_so_far;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
